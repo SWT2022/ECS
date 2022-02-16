2f83c2c3d3360eff5895cf48265f7203c21534a6
magnusblaa <magnus.moeller@hotmail.com>
2022-02-16 16:03:44 +0100

    ReDesign tilføjet

---
+++ ReDesignECS/FakeTempSensor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReDesignECS
+{
+    internal class FakeTempSensor : ITempSensor
+    {
+        public int GetTemp(int temp)
+        {
+            return temp;
+        }
+    }
+}
---
+++ ReDesignECS/Heater.cs
@@ -0,0 +1,29 @@
+﻿namespace ReDesignECS
+{
+
+    interface IHeater
+    { 
+        void TurnOn();
+        void TurnOff();
+    }
+
+    public class Heater: IHeater
+    {
+
+
+        public void TurnOn()
+        {
+            System.Console.WriteLine("Heater is on");
+        }
+
+        public void TurnOff()
+        {
+            System.Console.WriteLine("Heater is off");
+        }
+
+        public bool RunSelfTest()
+        {
+            return true;
+        }
+    }
+}
